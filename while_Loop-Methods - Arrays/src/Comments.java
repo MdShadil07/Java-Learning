57c922708e0fec291d0443bb065ceb4e468e2853
MdShadil07 <mdshadil62@gmail.com>
2025-01-14 19:53:39 +0530

    Allfiles

---
+++ while_Loop-Methods - Arrays/src/Comments.java
@@ -0,0 +1,18 @@
+public class Comments {
+    public static void main(String[] args) {
+        //This is a single line comments which will never be executed in the code
+        System.out.println("Hello, World!");
+
+        /* this is a multiple line code , comments are used for makind or taking the notes of the code for
+        better readibility and understanding of the code
+         */
+
+
+        /**
+         * this is java docs
+         * This is my main method
+         * @param args
+         *
+         */
+    }
+}
